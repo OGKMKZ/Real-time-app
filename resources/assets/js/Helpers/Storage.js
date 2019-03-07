67ea9543682d6c22ccba4dcfb6c24d8fb66f0c96
OG <babacarkane97@gmail.com>
2019-03-07 13:23:44 +0000

    Helpers Set

---
+++ resources/assets/js/Helpers/Storage.js
@@ -0,0 +1,21 @@
+class Storage {
+  store(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', user);
+  }
+
+  clear() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  getUser() {
+    return localStorage.getItem('user');
+  }
+}
+
+export default (Storage = new Storage());
---
+++ resources/assets/js/Helpers/User.js
@@ -0,0 +1,53 @@
+import Token from './Token';
+import Storage from './Storage';
+
+class User {
+  login(data) {
+    axios
+      .post('api/auth/login', data)
+      .then(response => this.handleResponse(response))
+      .catch(error => console.log(error.response.data));
+  }
+
+  handleResponse(res) {
+    const access_token = res.data.access_token;
+    const username = res.data.user;
+    if (Token.isValid(access_token)) {
+      Storage.store(access_token, username);
+    }
+  }
+
+  hasToken() {
+    const storedToken = Storage.getToken();
+    if (storedToken) {
+      return Token.isValid(storedToken);
+    }
+
+    return false;
+  }
+
+  loggedIn() {
+    return this.hasToken();
+  }
+
+  logout() {
+    Storage.clear();
+  }
+
+  name() {
+    if (this.loggedIn()) {
+      return Storage.getUser;
+    }
+  }
+
+  id() {
+    if (this.loggedIn()) {
+      const payload = Token.payload(Storage.getToken());
+      if (payload) {
+        return payload.sub;
+      }
+    }
+  }
+}
+
+export default (User = new User());
